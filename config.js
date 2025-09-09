0cf896304eeaebdcf0dbc2b029ddbe58331e5c10
TanyaMashamba01 <mildredtanyamashamba01@gmail.com>
2025-09-09 07:20:38 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,15 +5,15 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "KSMD~Dw5llbDR#wkVdW-ESnMCiodYeM4tpbSCAgz6E7O7LRhMYvTavngI",
+SESSION_ID: process.env.SESSION_ID || "KSMD~jhh0hDga#Z7KNHZ_s7VDJmz2hB9onSvnHTtQ-hcIiSSsd-WyDdp8",
 // add your Session Id 
-AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
+AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
 // make true or false status auto seen
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
-AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "false",
 // make true if you want auto reply on status 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*𝙰𝚄𝚃𝙾 𝚂𝙴𝙴𝙽 𝚈𝙾𝚄𝚁 𝚂𝚃𝙰𝚃𝚄𝚂 𝙱𝚈 <| 𝐊𝐈𝐍𝐆-𝐒𝐀𝐍𝐃𝐄𝐒𝐇-𝐌𝐃 𝐕❷🫧*",
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*𝙰𝚄𝚃𝙾 𝚂𝙴𝙴𝙽 𝚈𝙾𝚄𝚁 𝚂𝚃𝙰𝚃𝚄𝚂 𝙱𝚈 <| 𝐊𝐈𝐍𝐆-Josh-𝐌𝐃 𝐕❷🫧*",
 // set the auto reply massage on status reply  
 ANTI_DELETE: process.env.ANTI_DELETE || "true",
 // set true false for anti delete     
@@ -27,13 +27,13 @@ ANTI_LINK: process.env.ANTI_LINK || "false",
 // make anti link true,false for groups 
 MENTION_REPLY: process.env.MENTION_REPLY || "true",
 // make true if want auto voice reply if someone menetion you 
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/m5drmn.png",
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://i.postimg.cc/4NdSqms8/MidKing.jpg",
 // add custom menu and mention reply image url
-PREFIX: process.env.PREFIX || ".",
+PREFIX: process.env.PREFIX || "+",
 // add your prefix for bot   
-BOT_NAME: process.env.BOT_NAME || "<| 𝐊𝐈𝐍𝐆-𝐒𝐀𝐍𝐃𝐄𝐒𝐇-𝐌𝐃 𝐕❷",
+BOT_NAME: process.env.BOT_NAME || "<| 𝐊𝐈𝐍𝐆-JOSH-𝐌𝐃 𝐕❷",
 // add bot name here for menu
-STICKER_NAME: process.env.STICKER_NAME || "<| 𝐊𝐈𝐍𝐆-𝐒𝐀𝐍𝐃𝐄𝐒𝐇-𝐌𝐃 𝐕❷ | ꜱᴛɪᴄᴋᴇʀ ᴍᴀᴋᴇʀ",
+STICKER_NAME: process.env.STICKER_NAME || "<| ‎ʟᴇɢᴇɴᴛ ᴊᴏsʜ",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
@@ -41,21 +41,21 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links without remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "94741259325",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "263716729222",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "𝐌𝐑 𝐒ᴀɴᴅᴇꜱ𝐇 𝐁ʜᴀꜱʜᴀɴ𝐀",
+OWNER_NAME: process.env.OWNER_NAME || "ᴊᴏsʜᴋɪɴɢ ғʀᴏᴍ ᴢɪᴍ",
 // add bot owner name
-LOCATION: process.env.LOCATION || "Matara,Sri Lanka",
+LOCATION: process.env.LOCATION || "Zimbabwe,Gweru",
 // add bot location
-GITHUB: process.env.GITHUB || "https://github.com/vijitharanawakage",
+GITHUB: process.env.GITHUB || "https://github.com/TanyMashamba01",
 // add bot owner github
-EMAIL: process.env.EMAIL || "mrsandeshbhashana@gmail.com",
+EMAIL: process.env.EMAIL || "joshuamamboztech@gmail.com",
 // add bot owner email
-FOOTER: process.env.FOOTER || "> *© Powered By King-Sandesh-Md V2 💸*",
+FOOTER: process.env.FOOTER || "> *© Powered By 𝕜𝕚𝕟𝕘-𝕁𝕠𝕤𝕙-Md 𝕍𝟙 💸*",
 // add bot footer for menu  
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/uhys6h.png",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.postimg.cc/4NdSqms8/MidKing.jpg",
 // add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || " *YEP THIS IS KING-SANDESH-MD*⚡",
+LIVE_MSG: process.env.LIVE_MSG || " *YEP THIS IS KING-JOSH-MD*⚡",
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
@@ -69,19 +69,19 @@ ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
 // make anti link true,false for groups 
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 // make true for automatic stickers 
-AUTO_VOICE: process.env.AUTO_VOICE || "true",
+AUTO_VOICE: process.env.AUTO_VOICE || "false",
 // make true for automatic voice reply 
-AUTO_REPLY: process.env.AUTO_REPLY || "true",
+AUTO_REPLY: process.env.AUTO_REPLY || "false",
 // make true or false automatic text reply 
-AUTO_BIO: process.env.AUTO_BIO || "true",
+AUTO_BIO: process.env.AUTO_BIO || "false",
 // make true or false automatic about system 
 AUTO_NEWS: process.env.AUTO_NEWS || "false",
 // make true or false automatic news system 
-AUTO_NEWS_NUMBER: process.env.AUTO_NEWS_NUMBER || "94741259325",
+AUTO_NEWS_NUMBER: process.env.AUTO_NEWS_NUMBER || "263777756184",
 // enter your number with country code for automatic news system 
-THUMB_IMAGE: process.env.THUMB_IMAGE || "https://files.catbox.moe/m5drmn.png",
+THUMB_IMAGE: process.env.THUMB_IMAGE || "https://files.catbox.moe/cik12y.jpg",
 // thumb img for auto news system if has not photo
-ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
 // maks true for always online 
 BUTTON: process.env.BUTTON || "true",
 // true or false for button reply
@@ -91,7 +91,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "true",
 // true if want mark commands as read 
-DEV: process.env.DEV || "94741259325",
+DEV: process.env.DEV || "263777756184",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
